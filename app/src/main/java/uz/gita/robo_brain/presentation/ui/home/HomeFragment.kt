b60fab04b8ee24a910c4c58497854de93e94225a
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-08-20 00:17:58 +0500

    home ui finish

--- app/src/main/java/uz/gita/robo_brain/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/uz/gita/robo_brain/presentation/ui/home/HomeFragment.kt
@@ -1,33 +1,9 @@
 package uz.gita.robo_brain.presentation.ui.home
 
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import uz.gita.robo_brain.R
-import uz.gita.robo_brain.presentation.ui.home.view_model.HomeViewModel
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
-
-    private lateinit var viewModel: HomeViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
--- app/src/main/java/uz/gita/robo_brain/presentation/ui/puzzle_2048/screens/Puzzle2048Fragment.kt
+++ app/src/main/java/uz/gita/robo_brain/presentation/ui/puzzle_2048/screens/Puzzle2048Fragment.kt
@@ -1,9 +1,13 @@
 package uz.gita.robo_brain.presentation.ui.puzzle_2048.screens
 
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import uz.gita.robo_brain.R
+import uz.gita.robo_brain.databinding.FragmentPuzzle2048Binding
 
 class Puzzle2048Fragment : Fragment(R.layout.fragment_puzzle2048) {
+
+    private val binding: FragmentPuzzle2048Binding by viewBinding()
 
 
 }
