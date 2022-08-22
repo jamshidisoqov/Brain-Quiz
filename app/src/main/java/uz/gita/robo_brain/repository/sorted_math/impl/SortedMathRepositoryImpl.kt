da65cd61de708c3775d6195639dabac4421c4184
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-08-22 18:29:18 +0500

    sorted math repository added

--- app/src/main/java/uz/gita/robo_brain/repository/sorted_math/impl/SortedMathRepositoryImpl.kt
+++ app/src/main/java/uz/gita/robo_brain/repository/sorted_math/impl/SortedMathRepositoryImpl.kt
@@ -6,10 +6,16 @@ import uz.gita.robo_brain.utils.GenerateMathQuiz.generate
 
 class SortedMathRepositoryImpl : SortedMathRepository {
     override fun getQuestions(question: Int): List<SortedMath> {
-        var list = ArrayList<SortedMath>()
-        for (i in 0..4){
-            list.add(generate(question/5).toSortedMath())
+        val list = ArrayList<SortedMath>()
+        for (i in 0..4) {
+            list.add(generate(question / 5).toSortedMath())
         }
-        return emptyList()
+        list.sortBy {
+            it.answer
+        }
+        for (i in 0..4) {
+            list[i] = list[i].copy(order = i)
+        }
+        return list
     }
 }
