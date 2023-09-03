1524244a17bb52b650b8646c5cecac894190d462
jamshid <@jisoqov>
2023-09-03 19:50:35 +0500

    package name changed

--- app/src/main/java/uz/uni_team/robo_brain/utils/MovementDetector.kt
+++ app/src/main/java/uz/uni_team/robo_brain/utils/MovementDetector.kt
@@ -12,28 +12,26 @@ import kotlin.math.abs
 class MovementDetector(ctx: Context) : View.OnTouchListener {
     private var listener: ((Movement) -> Unit)? = null
     private val move = 80
-    private val gesture =
-        GestureDetector(ctx, object : GestureDetector.SimpleOnGestureListener() {
-            override fun onFling(
-                e1: MotionEvent,
-                e2: MotionEvent,
-                velocityX: Float,
-                velocityY: Float
-            ): Boolean {
-                if (abs(e1.x - e2.x) > abs(e1.y - e2.y)) {
-                    if (abs(e1.x - e2.x) < move) return false
-                    listener?.invoke(if (e2.x > e1.x) Movement.RIGHT else Movement.LEFT)
-                } else {
-                    if (abs(e1.y - e2.y) < move) return false
-                    listener?.invoke(if (e2.y > e1.y) Movement.DOWN else Movement.UP)
-                }
-                return true
+    private val gesture = GestureDetector(ctx, object : GestureDetector.SimpleOnGestureListener() {
+        override fun onFling(
+            e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float
+        ): Boolean {
+            if (abs(e1.x - e2.x) > abs(e1.y - e2.y)) {
+                if (abs(e1.x - e2.x) < move) return false
+                listener?.invoke(if (e2.x > e1.x) Movement.RIGHT else Movement.LEFT)
+            } else {
+                if (abs(e1.y - e2.y) < move) return false
+                listener?.invoke(if (e2.y > e1.y) Movement.DOWN else Movement.UP)
             }
-        })
+            return true
+        }
+    })
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
-        gesture.onTouchEvent(p1)
+        p1?.let {
+            gesture.onTouchEvent(p1)
+        }
         return true
     }
 
